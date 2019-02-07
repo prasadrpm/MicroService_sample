e4bdb8e15d8b7bb396a1f01f5bc67adb40baf1a9
Arun-Janani <Arun-Janani@AJS-World>
2019-02-07 22:08:19 +0530

    Zuul proxy Enabled

--- server1/src/main/java/com/eurekaServer/Server1Application.java
+++ server1/src/main/java/com/eurekaServer/Server1Application.java
@@ -3,9 +3,11 @@ package com.eurekaServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableZuulProxy
 public class Server1Application {
 
 	public static void main(String[] args) {
